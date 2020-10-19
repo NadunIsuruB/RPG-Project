39c92edae0ce79a183f43189e23b0efc107b0832
Nadun <Nadun@githhub.com>
2020-10-20 03:59:55 +0530

    Environment | Camera | controlling script and nav meshes

---
+++ RPG Project - Brackeys/Assets/Scripts/CamController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public Vector3 offSet;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ RPG Project - Brackeys/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask mask;
+
+    Camera cam;
+
+    PlayerMotor motor;
+    
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit , 100 , mask))
+            {
+                motor.MoveToPoint(hit.point);
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                //
+            }
+        }
+    }
+}
